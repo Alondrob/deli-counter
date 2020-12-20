c2cc1b276c4d67c39a15713c6c201809f1317e03
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-12-20 15:24:09 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+def line(array) #array of people
+    if array.empty?
+        puts "The line is currently empty."
+    else 
+        message = "The line is currently:"
+        array.each_with_index do |name, i|
+            message += " #{i.to_i+1}. #{name}"
+        end
+    puts "#{message}"
+    end
+            
+def take_a_number(array, name) # (array of the current line , name - last person joining the line)
+    array.push(name) # It pushes the names to the end of the arry
+    position = array.index(name) # this method gives the number in the array of an element 
+    puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+    return name, position 
+    end 
+end 
+
+def now_serving(array)
+    if array.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{array.shift}."
+    end 
+end 
---
+++ deli_training.rb
@@ -0,0 +1,17 @@
+
+
+#[5, 10, 15, 20, 25, 30].each_with_index do |num, idx|
+#p "#{num} is number #{idx} in the array"
+#end
+
+ a = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"].index("Ruchi")
+
+# p a    
+
+ #p a.push("peter")
+
+ b = [ "a", "b", "c" ]
+ b.push("D")
+b.shift
+
+p b 
